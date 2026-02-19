08f287e9209bb6e794fa51bd391f38082b2ed9c4
Danvitha-D <181647507+Danvitha-D@users.noreply.github.com>
2026-02-19 22:26:09 +0530

    Delete Prime.py

--- Prime.py
+++
@@ -1,8 +0,0 @@
-num =int(input("Enter number"))
-if num>1:
-    for i in range(2,num):
-        if num%i == 0:
-            print(" Number is not Prime")
-            break
-    else:
-        print("Number is Prime")
